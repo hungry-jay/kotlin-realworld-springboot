0248f9612c3536f7e5bb85fdd7d3bbca735d7e56
hungry-jay <49271821+hungry-jay@users.noreply.github.com>
2021-02-15 23:33:29 +0900

    feat: user repository 추가

---
+++ back/src/main/kotlin/back/repository/UserRepository.kt
@@ -0,0 +1,17 @@
+package back.repository
+
+import back.model.User
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : CrudRepository<User, Long> {
+//    count()	개수 확인
+//    delete()	1건 또는 여러 건 삭제
+//    deleteAll()	모두 삭제
+//    exists()	ID 있는지 확인
+//    findAll()	전체 또는 ID목록으로 조회
+//    findOne()	ID로 조회
+//    save()	1건 또는 여러 건 저장 return 저장한것
+
+}
